c4856c6c4e50b9c08a4d0205e1f1771664a12156
HiqueSteves <102489986+HiqueSteves@users.noreply.github.com>
2025-05-27 16:52:57 -0300

    Add files via upload

--- loja/index.js
+++ loja/index.js
@@ -1,21 +1,138 @@
 const express = require('express')
+const mysql = require('mysql2')
 
 const app = express()
 
 app.use(express.json())
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    database: 'loja'
+})
 
 app.get('/', (req, res) => {
     res.send("Servidor Express funcionando!")
 })
 
 app.post('/produtos', (req, res) => {
-    const nomeProduto = req.body.nomeProduto
+    const nome = req.body.nome
     const preco = req.body.preco
-    const quantidadeEstoque = req.body.quantidadeEstoque
+    const quantidade = req.body.quantidade
+
+    conexao.query(
+        'INSERT INTO produtos (nome, preco, quantidade) VALUES (?,?,?)',
+        [
+            nome,
+            preco,
+            quantidade,
+        ],
+        () => {
+        res.status(201).send('Consulta cadastrada com sucesso!')
+    })
 })
 
 app.get('/produtos', (req, res) => {
-    res.send("Nome: "+nomeProduto+"\n Preco: "+preco+"\n Quantidade no estoque: "+quantidadeEstoque)
+    conexao.query('SELECT * FROM produtos', (err, results) => {
+        if (err) {
+            res.status(500).send('Erro ao buscar produtos')
+        }
+
+        res.status(200).send(results)
+    })
+})
+
+app.delete('/produtos/:id', (req, res) => {
+    const { id } = req.params;
+
+    conexao.query('DELETE FROM produtos WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            return res.status(500).send('Erro ao deletar');
+        }
+        if (results.affectedRows === 0) {
+            return res.status(404).send('Produto não encontrado');
+        }
+
+        res.status(200).send('Produto deletado com sucesso!');
+    });
+});
+
+app.put('/produtos/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, preco, quantidade } = req.body;
+
+    const query = 'UPDATE produtos SET nome = ?, preco = ? WHERE id = ?';
+    conexao.query(query, [nome, preco, quantidade, id], (err, results) => {
+        if (err) {
+            return res.status(500).send('Erro ao atualizar');
+        }
+
+        if (results.affectedRows === 0) {
+            return res.status(404).send('Produto não encontrado');
+        }
+
+        res.send('Produto atualizado com sucesso');
+    })
+})
+
+app.post('/funcionarios', (req, res) => {
+    const { nome, cargo, salario} = req.body
+
+    conexao.query(
+        'INSERT INTO funcionarios (nome, cargo, salario) VALUES (?,?,?)',
+        [
+            nome,
+            cargo,
+            salario,
+        ],
+        () => {
+        res.status(201).send('Funcionario cadastrado com sucesso!')
+    })
+})
+
+app.delete('/funcionarios/:id', (req, res) => {
+    const { id } = req.params;
+
+    conexao.query('DELETE FROM funcionarios WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            return res.status(500).send('Erro ao deletar')
+        }
+        if (results.affectedRows === 0) {
+            return res.status(404).send('Funcionario não encontrado');
+        }
+
+        res.status(200).send('Funcionario deletado com sucesso');
+    })
+
+})
+
+app.put('/funcionarios/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, cargo, salario} = req.body;
+
+    const query = 'UPDATE funcionarios SET nome = ?, cargo = ? WHERE id = ?';
+    conexao.query(query, [nome, cargo, salario, id], (err, results) => {
+        if (err) {
+            return res.status(500).send('Erro ao atualizar');
+        }
+
+        if (results.affectedRows === 0) {
+            return res.status(404).send('Funcionario não encontrado');
+        }
+
+        res.send('Funcionario atualizado com sucesso')
+    })
+})
+
+app.get('/funcionarios', (req, res) => {
+    conexao.query('SELECT * FROM funcionarios', (err, results) => {
+        if (err) {
+            res.status(500).send('Erro ao buscar funcionario')
+        }
+
+        res.status(200).send(results)
+    })
 })
 
 app.listen(3000, () => {
