0c11efff4f3cb724e922e1553e3e93098f6dd929
HiqueSteves <102489986+HiqueSteves@users.noreply.github.com>
2025-08-13 16:49:46 -0300

    Add files via upload

---
+++ meuSiteTech/src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/global.css'
+import './components/NavBar/NavBar'
+
+function App() {
+  return (
+    <>
+      <h1>LojaTech</h1>
+    </>
+  );
+}
+
+export default App
---
+++ meuSiteTech/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import './navbar.css'
+
+function NavBar() {
+    return (
+        <>
+            <header>
+                <nav class="navbar">
+                    <div class="logo">LojaTech</div>
+                    <ul class="nav-links">
+                        <li><a href="#">Produtos</a></li>
+                        <li><a href="#">Ofertas</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+        </>
+    )
+}
+
+export default NavBar;
